32c42fa63522a3dd20acab757806570e4c03a6bd
Marcel Pennewiß <github@pennewiss.de>
2021-02-06 17:50:21 +0100

    Add mqtt state detection

--- status_openwrt/lib/state.sh
+++ status_openwrt/lib/state.sh
@@ -3,6 +3,24 @@
 # Get open state
 open_state() {
 
+  case $STATE_DETECTION in
+    "dhcp")
+      OPEN=$(open_state_devices)
+      ;;
+    "mqtt")
+      OPEN=$(open_state_mqtt)
+      ;;
+    *)
+      OPEN="Invalid state detection mode"
+      ;;
+  esac
+
+  echo $OPEN
+}
+
+# Get open state by active device count
+open_state_devices() {
+
   DEVICES=$(devices_count)
   case $DEVICES in
     "0") OPEN="false"
@@ -12,6 +30,26 @@ open_state() {
     *) OPEN="true"
        ;;
   esac
+
+  echo $OPEN
+}
+
+# Get open state by mqtt sensor
+open_state_mqtt() {
+
+  #MQTT_ANSWER=$(mosquitto_sub -h $MQTT_HOST -C 1 -u $MQTT_USER -P $MQTT_PASSWORD -t $MQTT_SENSOR 2>/dev/null)
+  MQTT_ANSWER="12"
+
+  if echo $MQTT_ANSWER | grep --quiet -E "$MQTT_STATE_OPEN"; then
+    # Open state
+    OPEN="true"
+  elif echo $MQTT_ANSWER | grep --quiet -E "$MQTT_STATE_CLOSED"; then
+    # Closed state
+    OPEN="false"
+  else
+    # Unkown state
+    OPEN="false"
+  fi
 
   echo $OPEN
 }
